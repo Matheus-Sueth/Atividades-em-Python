6b92437777bdc5cf7894d0562db9d20a0e06f78b
Matheus-Sueth <75100700+Matheus-Sueth@users.noreply.github.com>
2021-04-06 01:14:34 -0300

    Create Exercicio1.1.py

---
+++ Exercicio1.1.py
@@ -0,0 +1,14 @@
+"""
+
+1.1 Lista de números positivos terminada por 0, imprimir seus quadrados
+
+"""
+
+lista = []
+i = 0
+n = int(input('Digite quantos números você vai digitar: '))
+while (i < n):
+  lista.append(int(input('Digite ' + str(i+1) + '° número: ')))
+  i+=1
+for i in lista:
+  print(i**2)
