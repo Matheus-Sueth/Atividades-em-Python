1111f49fdbf07420d11d5a64ed126d9d0e4a75f8
Matheus-Sueth <75100700+Matheus-Sueth@users.noreply.github.com>
2021-04-06 01:06:50 -0300

    Update polinomio.py

--- polinomio.py
+++ polinomio.py
@@ -16,12 +16,10 @@ poten = grau = int(input(' '))
 i = 0
 while (i <= grau):
   a.append(float(input('Digite o ' + str(i+1) + '°coeficiente: ')))
-  print(str(a[i]))
   if (i == grau):
     x = float(input('Digite o valor da variável: '))
   i+=1
 if (grau == 1):
-  print(str(a[0]) + " * " + str(x) + " + " + str(a[1]))
   f = a[0]*(x**(poten)) + a[1]
 elif (grau == 2):
   f = a[0]*(x**poten) + a[1]*(x**(poten-1)) + a[2]
